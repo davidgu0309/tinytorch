a0285bfed4ed16aeb38c6e55ac112a1d4ff18615
mirco-paul <mirco.paul.freelance@gmail.com>
2025-01-23 13:47:23 +0100

    Print line at the end

--- test/graph_unit_tests.cpp
+++ test/graph_unit_tests.cpp
@@ -9,6 +9,7 @@ void graphUnitTests(){
     Graph<int> graph;
     for(int i = 1; i <= 5; ++i) graph.addNode(i);
     std::cout << "Size test 1: " << (graph.size() == 5 ? "Passed" : "Failed") << std::endl;
-}
+    std::cout << std::endl;
+}   
     
 } // namespace tinytorch
